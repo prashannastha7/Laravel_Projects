a5ffd7f70a144471480009ff4e847f59efbaf6f1
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-02-01 19:37:11 +0545

    added dashboard posted blog

--- MiniBlogging/resources/views/dashboard.blade.php
+++ MiniBlogging/resources/views/dashboard.blade.php
@@ -9,7 +9,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <table class="table-auto">
+                        <thead>
+                            <tr>
+                                <th class="px-4 py-2">Name</th>
+                                <th class="px-4 py-2">Title</th>
+                                <th class="px-4 py-2">Body</th>
+                                <th class="px-4 py-2">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($posts as $post)
+                            <tr>
+                                <td class="border px-4 py-2">{{ $post->user->name }}</td>
+                                <td class="border px-4 py-2">{{ $post->title }}</td>
+                                <td class="border px-4 py-2">{{ $post->body }}</td>
+                                <td class="border px-4 py-2">
+                                    <a href="" class="bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
+                                        Edit</a>
+                                    <a href="" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                        Delete
+</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
--- MiniBlogging/routes/web.php
+++ MiniBlogging/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,9 +25,8 @@ Route::post('/post', [PostController::class, 'create'])->name('post_create');
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'show_post']
+)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
