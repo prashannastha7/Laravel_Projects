6043c209c8128fcb865987d8e493016f91300f50
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-02-03 01:01:32 +0545

    display specific posts

--- MiniBlogging/app/Http/Controllers/DashboardController.php
+++ MiniBlogging/app/Http/Controllers/DashboardController.php
@@ -7,9 +7,11 @@ use App\Models\Post;
 
 class DashboardController extends Controller
 {
-    public function show_post()
+    public function show_post(Request $request)
     {
-        $posts = Post::all();
+        //$posts = Post::all();
+        $userid = $request->user()->id;
+        $posts = Post::where('user_id', $userid)->get();
         return view('dashboard', ['posts' => $posts]);
     }
 }
